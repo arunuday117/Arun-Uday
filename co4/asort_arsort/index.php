deecf296221aa967c0d09a4c3e4165ca99ab10bb
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2022-01-19 22:10:51 +0530

    Add files via upload

---
+++ co4/asort_arsort/index.php
@@ -0,0 +1,11 @@
+<?php
+    $student=array("Ajin","Abhi","Sanoop","Manu","Nikhil");
+    print_r($student);
+    echo"<br>";
+    asort($student);
+    echo"asort : ";
+    print_r($student);
+    echo"<br>arsort : ";
+    arsort($student);
+    print_r($student);
+?> 
